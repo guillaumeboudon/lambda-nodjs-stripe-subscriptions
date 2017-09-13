8302d9e2e00a00f9cd3b0aa3e273f1cefa9bcb9d
Thibaut Assus <thibaut@milesrock.com>
2017-09-13 12:34:13 +0200

    Test

--- handler.js
+++ handler.js
@@ -2,37 +2,18 @@
 
 require('dotenv').config()
 
-module.exports.stripePayment = (event, context, callback) => {
+module.exports.stripePayment = (jsonData, context, callback) => {
   const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-  try {
-    // Parse Stripe Event
-    const jsonData = JSON.parse(event.body); // https://stripe.com/docs/api#event_object
-
-    // Verify the event by fetching it from Stripe
-    console.log("Stripe Event: %j", jsonData);
-    stripe.events.retrieve(jsonData.id, (err, stripeEvent) => {
-      const eventType = stripeEvent.type ? stripeEvent.type : '';
-      const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: 'Stripe webhook incoming!',
-          stage: requestContextStage,
-        }),
-      };
-      console.log("Event Type: %j", eventType);
-
-      // Branch by event type
-      switch (eventType) {
-        case 'invoice.created':
-          // invoice.created event
-          break;
-        default:
-          break;
-      }
-      callback(null, response);
-    });
-  } catch (err) {
-    context.fail(err);
-  }
+  stripe.customers.create({
+    email: jsonData.stripeEmail,
+    source: jsonData.stripeToken
+  }).then(function(customer){
+    stripe.subscriptions.create({
+      customer: customer.id,
+      items: [{
+        plan: jsonData.plan
+      }]
+    })
+  })
 };
