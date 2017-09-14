57a7d924bd6b6754fcefcdee7c5512d748268204
Guillaume BOUDON <guillaumeboudon@gmail.com>
2017-09-14 11:49:56 +0200

    Start implementing callback

--- handler.js
+++ handler.js
@@ -1,23 +1,38 @@
 'use strict';
 
-require('dotenv').config()
+var returner = function(callback, stripeErrors) {
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+      "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+    },
+    body: JSON.stringify({ "message": stripeErrors })
+  };
+  return callback(null, response);
+}
 
 module.exports.stripePayment = (event, context, callback) => {
   const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-  const queryString = require('query-string')
-
-  const jsonData = queryString.parse(event.body);
+  const body = JSON.parse(event.body);
 
   stripe.customers.create({
-    email: jsonData.stripeEmail,
-    source: jsonData.stripeToken
+    email: body.stripeEmail
   }).then(function(customer){
-    stripe.subscriptions.create({
+    return stripe.customers.createSource(customer.id, {
+      source: "toto"/*body.stripeToken*/
+    });
+  }).then(function(customer) {
+    return stripe.subscriptions.create({
       customer: customer.id,
       items: [{
-        plan: jsonData.plan
+        plan: body.stripePlanId
       }]
-    })
-  })
+    });
+  }).catch(function(err) {
+    return err
+  });
+
+  return returner(callback, "");
 };
